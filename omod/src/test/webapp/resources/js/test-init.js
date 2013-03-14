0d9a4b107b52559c9e68dc9c6cc7038813268235
Daniel Shorten <danielshorten@gmail.com>
2013-03-14 16:26:37 +0300

    Rearranged test init & util code.  Added util function to test a model's toJSON().

---
+++ omod/src/test/webapp/resources/js/test-init.js
@@ -0,0 +1,7 @@
+window.openhmis = {};
+openhmis.url = {
+	//wwwUrlRoot: "/target/classes/web/module/resources/", // Test minified JS
+	backboneBase: "",
+	resources: "/src/main/webapp/resources/",
+	rest: "/rest/"
+}
